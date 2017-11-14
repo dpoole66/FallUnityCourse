5875b8f24d3b43483585a17463ad818d11c890b3
dpoole66 <donpoole66@gmail.com>
2017-11-14 09:16:43 -0800

    Mettle and Enemy setup for Melee

---
+++ Mettle/Assets/_Scripts/AI_Control/Battle/MeleeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeManager : MonoBehaviour {
+
+    public Camera MettleCam;
+    public float range;
+    public LayerMask thisLayerMask;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire2")) {
+            Attack(); 
+        }
+	}
+
+    void Attack() {
+        RaycastHit hit;
+        if (Physics.Raycast(MettleCam.transform.position, MettleCam.transform.forward, out hit, range, thisLayerMask)) {
+
+            Debug.Log(hit.transform.name);
+
+        }
+    }
+}
